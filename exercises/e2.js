67134260a1aca09329455e34d3305b058ef0158f
Clester Ricky Jackson <jackson102890@gmail.com>
2025-04-22 07:38:02 -0600

    Find sq root of all numbers from loop

--- exercises/e2.js
+++ exercises/e2.js
@@ -3,7 +3,14 @@
 // getNumbersWithSquareRoots(17) => [0, 1, 4, 9, 16]
 
 export function getNumbersWithSquareRoots(max) {
-  // Your code goes here...
+  const sqRootNums = [];
+  for (let i = 0; i < max; i++) {
+    if (i * i == i) {
+      sqRootNums.push(i);
+      return sqRootNums;
+    }
+    
+  }
 
 }
 
